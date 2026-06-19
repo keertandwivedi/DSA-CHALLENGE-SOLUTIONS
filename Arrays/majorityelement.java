7ccd9c6272c6e8a9c207a56192b9d2a466cb7ca3
Your Name <you@example.com>
2026-06-20 00:30:25 +0530

    majority elemet in array >n/3  times

---
+++ Arrays/majorityelement.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main {
+
+    public static List<Integer> majorityElement(int[] nums) {
+        int count1 = 0, count2 = 0;
+        int candidate1 = 0, candidate2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            }
+            else if (num == candidate2) {
+                count2++;
+            }
+            else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            }
+            else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            }
+            else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Verification pass
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+
+        if (count1 > n / 3) result.add(candidate1);
+        if (count2 > n / 3) result.add(candidate2);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 200, 2, 2, 4, 3, 1, 2, 1, 2, 1, 1};
+
+        System.out.println(majorityElement(a));
+    }
+}
